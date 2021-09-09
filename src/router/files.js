996d18ab474408904c89e14b6b9f77f97fef6e12
Spiderjockey2 <benjamin.forey11gmail.com>
2021-09-09 12:29:18 +0100

    Added support for files with special characters

--- src/router/files.js
+++ src/router/files.js
@@ -3,33 +3,42 @@ const express = require('express'),
 	dirTree = require('directory-tree'),
 	fresh = require('fresh');
 
+const location = process.cwd() + '/src/files';
+
+// Show file explorer
 router.get('/', (req, res) => {
 	const path = req.query.path ?? '/';
-	// console.log(process.cwd() + '/src/files' + path);
-	// console.log(dirTree(process.cwd() + '/src/files' + path));
+	console.log(location + path);
+	console.log(dirTree(location + path));
 	// console.log(path);
-	res.render('files', {
-		files: dirTree(process.cwd() + '/src/files' + path),
-		path: path,
-		formatBytes: function formatBytes(bytes, decimals = 2) {
-			if (bytes === 0) return '0 Bytes';
-			const k = 1024,
-				dm = decimals < 0 ? 0 : decimals,
-				sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
-				i = Math.floor(Math.log(bytes) / Math.log(k));
+	res
+		.status(200)
+		.render('files', {
+			files: dirTree(location + path),
+			path: path,
+			formatBytes: function formatBytes(bytes, decimals = 2) {
+				if (bytes === 0) return '0 Bytes';
+				const k = 1024,
+					dm = decimals < 0 ? 0 : decimals,
+					sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+					i = Math.floor(Math.log(bytes) / Math.log(k));
 
-			return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
-		},
-	});
+				return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+			},
+		});
 });
 
+// Show file
 router.get('/*', (req, res) => {
+	// Read file from cache
 	if (isFresh(req, res)) {
 		res.statusCode = 304;
 		res.end();
 		return;
 	}
-	res.status(200).sendFile(process.cwd() + '/src/files' + (req.query.path ?? '') + req.url.split('?')[0], (err) => {
+
+	// new file
+	res.status(200).sendFile(decodeURI(location + (req.query.path ?? '') + req.url.split('?')[0]), (err) => {
 		if (err) return res.status(404).end('content not found.');
 	});
 });
