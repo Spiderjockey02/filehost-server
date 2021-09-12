5e07f17c8e3239f6b13b8b07c46a29cabc1c5f40
Spiderjockey02 <benjamin.forey11gmail.com>
2021-09-12 12:53:48 +0100

    updated config.example.js

--- src/config.example.js
+++ src/config.example.js
@@ -1,6 +1,10 @@
 const config = {
+	// The domain of the website
 	domain: 'URL',
+	// port the server will run on
 	port: 1111,
+	// the database for storing user info
+	MongoDBURl: 'mongodb://localhost:27017/filesharer',
 };
 
 module.exports = config;
