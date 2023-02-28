b45fc6b61fb701d0e17a9c4cabde9d8f5888dbee
Spiderjockey02 <benjamin.forey11@gmail.com>
2023-02-28 01:10:29 +0000

    Updated admin endpoint for linux support

--- storage-service/src/routes/api/admin.ts
+++ storage-service/src/routes/api/admin.ts
@@ -1,4 +1,3 @@
-// For upload, delete, move etc endpoints
 import { Router } from 'express';
 import { fetchAllUsers } from '../../db/User';
 import { fetchAllGroups } from '../../db/Group';
@@ -6,42 +5,50 @@ import directoryTree, { getNumberOfFiles } from '../../utils/directory';
 import { checkAdmin } from '../../utils/functions';
 import { exec } from 'node:child_process';
 import os from 'os';
+import util from 'node:util';
+const cmd = util.promisify(exec);
 const router = Router();
 
 type data = { [key: string]: boolean}
 
 export default function() {
-
 	router.get('/stats', checkAdmin, async (_req, res) => {
-		exec('wmic logicaldisk get size,freespace,caption', async function(_error, stdout) {
-			// Get and parse storage information
+
+		const platform = process.platform;
+		let diskData: Array<string> = [];
+		if (platform == 'win32') {
+			const { stdout } = await cmd('wmic logicaldisk get size,freespace,caption');
 			const parsed = stdout.trim().split('\n').slice(1).map(line => line.trim().split(/\s+(?=[\d/])/));
 			const filtered = parsed.filter(d => process.cwd().toUpperCase().startsWith(d[0].toUpperCase()));
-			const diskData = filtered[0];
+			diskData = filtered[0];
+		} else if (platform == 'linux') {
+			const { stdout } = await cmd('df -Pk --');
+			const parsed = stdout.trim().split('\n').slice(1).map(line => line.trim().split(/\s+(?=[\d/])/));
+			const filtered = parsed.filter(d => process.cwd().toUpperCase().startsWith(d[0].toUpperCase()));
+			diskData = filtered[0];
+		}
 
-			// Send data back to user
-			res.json({
-				storage: {
-					totalFiles: getNumberOfFiles(directoryTree(process.cwd()), 0),
-					total: Number(diskData[2]),
-					free: Number(diskData[1]),
-				},
-				memory: {
-					total: Number((process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)),
-					free:  Number((os.totalmem() / 1024 / 1024).toFixed(2)),
-				},
-				cpu: {
-					total: 0,
-					avg: os.loadavg(),
-				},
-				users: {
-					total: (await fetchAllUsers()).length,
-					groups: (await fetchAllGroups()).map(g => ({
-						name: g.name,
-						userCount: g._count.users,
-					})),
-				},
-			});
+		res.json({
+			storage: {
+				totalFiles: getNumberOfFiles(directoryTree(process.cwd()), 0),
+				total: Number(diskData[2] ?? 0),
+				free: Number(diskData[1] ?? 0),
+			},
+			memory: {
+				total: Number((process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)),
+				free:  Number((os.totalmem() / 1024 / 1024).toFixed(2)),
+			},
+			cpu: {
+				total: 0,
+				avg: os.loadavg(),
+			},
+			users: {
+				total: (await fetchAllUsers()).length,
+				groups: (await fetchAllGroups()).map(g => ({
+					name: g.name,
+					userCount: g._count.users,
+				})),
+			},
 		});
 	});
 
