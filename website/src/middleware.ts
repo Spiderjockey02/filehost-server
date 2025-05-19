46e818d5066f261b4f0117cbba80f34a37de9a25
Spiderjockey02 <benjamin.forey11@gmail.com>
2025-05-19 22:10:39 +0100

    Added /notifications & used nextJS fetch instead of axios

--- website/src/middleware.ts
+++ website/src/middleware.ts
@@ -1,27 +1,33 @@
 import { NextRequest, NextResponse } from 'next/server';
-import axios from 'axios';
 
 export async function middleware(request: NextRequest) {
 	// Fetch user session from the server
-	const { data } = await axios.get(`${request.nextUrl.origin}/api/auth/get-session`, {
-		headers: {
-			cookie: request.headers.get('cookie') || '',
-		},
-	});
+	try {
+		const res = await fetch(`${request.nextUrl.origin}/api/auth/get-session`, {
+			headers: {
+				cookie: request.headers.get('cookie') || '',
+			},
+		});
 
-	// Check to see if the user is logged in
-	if (data.user) {
-		if (request.nextUrl.pathname.startsWith('/admin')) {
-			if (data.user.role !== 'admin') {
-				return NextResponse.redirect(new URL('/', request.url));
+		const data = await res.json();
+
+		// Check to see if the user is logged in
+		if (data.user) {
+			if (request.nextUrl.pathname.startsWith('/admin')) {
+				if (data.user.role !== 'admin') {
+					return NextResponse.redirect(new URL('/', request.url));
+				}
 			}
+			return NextResponse.next();
+		} else {
+			return NextResponse.redirect(new URL('/', request.url));
 		}
-		return NextResponse.next();
-	} else {
+	} catch (err) {
+		console.log(err);
 		return NextResponse.redirect(new URL('/', request.url));
 	}
 }
 
 export const config = {
-	matcher: ['/files/:path*', '/settings', '/trash', '/search', '/recent', '/admin/:path*'],
+	matcher: ['/files/:path*', '/settings', '/trash', '/search', '/recent', '/admin/:path*', '/notifications'],
 };
