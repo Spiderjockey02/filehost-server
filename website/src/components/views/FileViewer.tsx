4ab2a1c045e0618345cf51b220fd76f1bfb0c130
Spiderjockey02 <benjamin.forey11@gmail.com>
2025-07-11 17:22:23 +0100

    No need for lookup, mimetype is a key of file

--- website/src/components/views/FileViewer.tsx
+++ website/src/components/views/FileViewer.tsx
@@ -1,13 +1,10 @@
 import { VideoPlayer, TextViewer } from '@/components';
-import MimeType from 'mime-types';
 import { useRef } from 'react';
 import Image from 'next/image';
 import type { FileViewerProps } from '@/types/Components/Views';
 
 export default function FileViewer({ file }: FileViewerProps) {
 	const imageRef = useRef<HTMLImageElement>(null);
-	const mimeType = MimeType.lookup(file.name);
-
 	function handleFullScreen() {
 		if (imageRef.current) {
 			if (!document.fullscreenElement) {
@@ -19,10 +16,10 @@ export default function FileViewer({ file }: FileViewerProps) {
 	}
 
 	// Handle text files or unknown mime types
-	if (!mimeType || mimeType.startsWith('text') || mimeType == 'application/javascript') return <TextViewer path={`/content/${file.userId}${file.path}`} />;
+	if (!file.mimetype || file.mimetype.startsWith('text') || file.mimetype == 'application/javascript' || file.mimetype == 'application/json') return <TextViewer path={`/content/${file.userId}${file.path}`} />;
 
 	// Handle PDF files
-	if (mimeType == 'application/pdf') {
+	if (file.mimetype == 'application/pdf') {
 		return (
 			<object data={`/content/${file.userId}${file.path}`} type="application/pdf" style={{ width: '100%', height: '80vh' }}>
 				<p>Alternative text - include a link <a href={`/content/${file.userId}${file.path}`}>to the PDF!</a></p>
@@ -30,22 +27,20 @@ export default function FileViewer({ file }: FileViewerProps) {
 		);
 	}
 
-	switch (mimeType.split('/')[0]) {
+	switch (file.mimetype.split('/')[0]) {
 		case 'image':
 			return (
 				<div className='d-flex justify-content-center' style={{ maxHeight: 'calc(100vh - 130px)', padding: '0 6px' }}>
-					<Image className="center" src={`/content/${file.userId}${file.path}`} onClick={handleFullScreen} ref={imageRef}
+					<Image className="center" src={`/content/${file.userId}${file.path}`} onClick={handleFullScreen} ref={imageRef} unoptimized={true}
 						alt={file.name} width={1000} height={1000} style={{ cursor: 'pointer', maxWidth: '100%', height: 'auto', objectFit: 'contain' }}
 					/>
 				</div>
 			);
 		case 'video':
 			return <VideoPlayer path={file.path} userId={file.userId} />;
-		case 'audio':
-		case 'application':
 		default:
 			<div className="text-center">
-				<p>Unsupported file type: {mimeType}</p>
+				<p>Unsupported file type: {file.mimetype}</p>
 				<a href={`/content/${file.userId}${file.path}`} className="btn btn-primary">Download File</a>
 			</div>;
 	}
