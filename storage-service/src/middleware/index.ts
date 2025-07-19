b824d47680373b1152ed2bb8f913af14e2bbab94
Spiderjockey02 <benjamin.forey11@gmail.com>
2025-07-19 17:27:46 +0100

    Cleaned up middleware + express@5 does not allow returns so updated

--- storage-service/src/middleware/index.ts
+++ storage-service/src/middleware/index.ts
@@ -4,34 +4,47 @@ import parseForm from './parse-form';
 import { Error } from '../utils';
 import Client from 'src/helpers/Client';
 import { IncomingHttpHeaders } from 'node:http';
+import { FullSession } from 'src/types/database/Session';
 
-export async function getSession(client: Client, headers: IncomingHttpHeaders) {
+/**
+  * Fetches the session from the request headers.
+  * @param client - The Client instance
+  * @param headers - The request headers
+  * @return {Promise<Session | null>} - The session object or null if not found
+*/
+export async function getSession(client: Client, headers: IncomingHttpHeaders): Promise<FullSession | null> {
+	// Get the session token from the cookies
 	if (headers.cookie == undefined) return null;
-
-	// get Session token from cookies
 	const cookies = headers['cookie'].split('; ');
-	const parsedcookies = cookies.map((i: string) => i.split('='));
-
-	// Get session token (Could be secure or not so check both)
-	const sessionToken = parsedcookies.find(i => i[0] == 'better-auth.session_token')?.[1];
+	const parsedCookies = cookies.map((i: string) => i.split('='));
+	const sessionToken = parsedCookies.find(i => i[0] == 'better-auth.session_token')?.[1];
 	if (!sessionToken) return null;
 
+	// Fetch the session using the session token
 	try {
-		const session = await client.sessionManager.fetchByToken(sessionToken.split('.')[0]);
-		return session;
+		return client.sessionManager.fetchByToken(sessionToken.split('.')[0]);
 	} catch (err) {
 		console.log(err);
 		return null;
 	}
 }
 
-export async function checkAdmin(client: Client) {
+/**
+  * Middleware to check if the user is logged in and has the admin role.
+  * @param client - The Client instance
+  * @return {Promise<(req: Request, res: Response, next: NextFunction) => Promise<void>} - Calls next middleware if the user is logged in, otherwise sends an error response
+*/
+export async function checkAdmin(client: Client): Promise<(req: Request, res: Response, next: NextFunction) => Promise<void>> {
 	return async (req: Request, res: Response, next: NextFunction) => {
 		const session = await getSession(client, req.headers);
-		if (session == null) return Error.InvalidSession(res);
+		if (session == null) {
+			Error.InvalidSession(res);
+			return;
+		}
 
 		if (session.user.role == 'admin') return next();
-		return Error.InvalidAccess(res);
+		Error.InvalidAccess(res);
+		return;
 	};
 }
 
