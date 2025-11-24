ba397c263d7b3c820431fc23947fa92b891ec7fe
Spiderjockey02 <benjamin.forey11@gmail.com>
2025-11-24 01:32:11 +0000

    Updated to eslint version 9

---
+++ storage-service/eslint.config.js
@@ -0,0 +1,55 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [{
+	ignores: ['**/dist/**', '**/node_modules/**'],
+	files: ['**/*.ts', '**/*.tsx', '**/*.js'],
+	languageOptions: {
+		parser: tsParser,
+		ecmaVersion: 2021,
+		sourceType: 'module',
+		globals: {
+			node: 'readonly',
+		},
+	},
+	plugins: {
+		'@typescript-eslint': tseslint,
+	},
+	rules: {
+		'brace-style': ['error', '1tbs', { allowSingleLine: true }],
+		'comma-dangle': ['error', 'always-multiline'],
+		'comma-spacing': 'error',
+		'comma-style': 'error',
+		curly: ['error', 'multi-line', 'consistent'],
+		'dot-location': ['error', 'property'],
+		'handle-callback-err': 'off',
+		indent: ['error', 'tab', { SwitchCase: 1 }],
+		'max-nested-callbacks': ['error', { max: 6 }],
+		'max-statements-per-line': ['error', { max: 2 }],
+		'no-console': 'off',
+		'no-empty-function': 'error',
+		'no-floating-decimal': 'error',
+		'no-inline-comments': 'error',
+		'no-lonely-if': 'error',
+		'no-multi-spaces': 'error',
+		'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 1, maxBOF: 0 }],
+		'no-shadow': ['error', { allow: ['err', 'resolve', 'reject'] }],
+		'no-trailing-spaces': 'error',
+		'no-var': 'error',
+		'object-curly-spacing': ['error', 'always'],
+		'prefer-const': 'error',
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
+		'space-before-blocks': 'error',
+		'space-before-function-paren': ['error', {
+			anonymous: 'never',
+			named: 'never',
+			asyncArrow: 'always',
+		}],
+		'space-in-parens': 'error',
+		'space-infix-ops': 'error',
+		'space-unary-ops': 'error',
+		'spaced-comment': 'error',
+		yoda: 'error',
+	},
+}];
