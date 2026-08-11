e24d2f8d6213a1c7bf4b413945a47507f4642428
Spiderjockey02 <benjamin.forey11@gmail.com>
2026-08-11 20:57:29 +0100

    Utilised prismaSchemaFolder for better schema management

--- storage-service/prisma.config.ts
+++ storage-service/prisma.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, env, PrismaConfig } from 'prisma/config';
 import 'dotenv/config';
 
 export default defineConfig({
-	schema: 'prisma/schema.prisma',
+	schema: 'prisma/',
 	migrations: {
 		path: 'prisma/migrations',
 		seed: 'tsx prisma/seed.ts',
